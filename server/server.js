4db1d422e020bc65de3695f1e22371610150f560
MariaFernandaMarroquin <MariaFernanda@MacBook-Pro-de-Martha.local>
2023-08-08 16:08:38 -0600

    formato document

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const db = require('./config/connection');
 
 //New code
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth'); 
-const { typeDefs, resolvers } = require('./schemas'); 
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,9 +21,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
   //New code
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
 
